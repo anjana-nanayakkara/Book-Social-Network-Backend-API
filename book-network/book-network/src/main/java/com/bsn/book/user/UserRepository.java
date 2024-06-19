b8b9367b43492af4d14b1447e2a73790dbef6c16
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2024-06-19 16:27:34 +0530

    security config and jwt classes added

---
+++ book-network/book-network/src/main/java/com/bsn/book/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bsn.book.user;
+
+import com.bsn.book.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
